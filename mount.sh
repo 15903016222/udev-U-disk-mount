744e784fffe89c89fa25517c8a403781bbb32e6b
wangsheng <wangsheng@cndoppler.cn>
2018-04-11 17:54:41 +0800

    U盘无挂载，udev却表现已挂载

--- mount.sh
+++ mount.sh
@@ -9,54 +9,22 @@ name=${name%_*}
 
 DIR="/media/$name"
 
-echo "00000000" >> /tmp/test.txt
+i=0
 
 if [ "add" == $ACTION ]
 then
 	if ! cat /proc/mounts | awk '{print $1}' | grep -q "$DEVNAME"
 	then
-		echo "111111111" >> /tmp/test.txt
-		for i in $(seq 1 3)
+		while cat /proc/mounts | awk '{print $2}' | grep -q "$DIR" 
 		do
-			echo "44444444" >> /tmp/test.txt
-			if [ ! -d "$DIR" ]
-			then
-				echo "555555555" >> /tmp/test.txt
-				mkdir -p $DIR
-				break
-			else
-				echo "666666666" >> /tmp/test.txt
-				# 无匹配 返回0 执行
-				if ! cat /proc/mounts | awk '{print $2}' | grep -q "$DIR"
-				then
-					echo "---------" >> /tmp/test.txt
-					WANGSHENG=`$MOUNT | awk '{print $2}' | grep "$DIR"`
-					echo $WANGSHENG >> /tmp/test.txt
-					echo "777777777" >> /tmp/test.txt
-					break
-				# 有屁配 返回非0 执行
-				else
-					echo "********" >> /tmp/test.txt
-					WANGSHENG=`$MOUNT | awk '{print $2}' | grep "$DIR"`
-					echo $WANGSHENG >> /tmp/test.txt
-					echo "888888888" >> /tmp/test.txt
-					DIR="/media/$name$i"
-				fi
-			fi
+			echo "00000000" >> /tmp/test.txt
+			i=`expr $i + 1`
+			DIR="/media/$name$i"
 		done
 
-		# mount the U-disk
-		echo $DEVNAME >> /tmp/test.txt
-		echo $DIR >> /tmp/test.txt
-		$MOUNT -t auto -o async,relatime $DEVNAME $DIR > /tmp/log 2>&1
-		if $MOUNT $DEVNAME $DIR
-		then
-			echo "delete1" >> /tmp/test.txt
-			rmdir $DIR
-		else
-			echo "delete2" >> /tmp/test.txt
-			rmdir $DIR
-		fi
+		mkdir -p $DIR
+
+		$MOUNT $DEVNAME $DIR
 	else
 		return
 	fi
