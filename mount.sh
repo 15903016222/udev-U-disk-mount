46425240ed73d5ceb35a2e94a6a25875414170aa
wangsheng <wangsheng@cndoppler.cn>
2018-04-11 17:57:04 +0800

    修改挂载脚本

--- mount.sh
+++ mount.sh
@@ -9,22 +9,14 @@ name=${name%_*}
 
 DIR="/media/$name"
 
-i=0
-
 if [ "add" == $ACTION ]
 then
 	if ! cat /proc/mounts | awk '{print $1}' | grep -q "$DEVNAME"
 	then
-		while cat /proc/mounts | awk '{print $2}' | grep -q "$DIR" 
-		do
-			echo "00000000" >> /tmp/test.txt
-			i=`expr $i + 1`
-			DIR="/media/$name$i"
-		done
+		! test -d $DIR && mkdir -p $DIR
 
-		mkdir -p $DIR
-
-		$MOUNT $DEVNAME $DIR
+		# 发送挂载信号
+#		$MOUNT $DEVNAME $DIR
 	else
 		return
 	fi
