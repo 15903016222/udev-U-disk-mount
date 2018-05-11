34fbf7a25a2feb3242fbc87d89dc1fce2bbc111f
wangsheng <wangsheng@cndoppler.cn>
2018-05-11 11:01:22 +0800

    修复：U盘没有盘符的情况下，指定挂载路径；每个外接存储盘只挂载第一个分区

--- mount.sh
+++ mount.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 MOUNT="/bin/mount"
 UMOUNT="/bin/umount"
@@ -10,7 +10,31 @@ name=${name%_*}
 
 DIR="/media/$name"
 i=0
-if ! cat /proc/mounts | awk '{print $1}' | grep -q "$DEVNAME"
+
+DRIVER=$DEVNAME
+LAST_NUM=`echo $DRIVER | wc -L`
+LAST_NUM1=$LAST_NUM-1 
+DRIVER1=${DRIVER:LAST_NUM1:LAST_NUM}
+DRIVER2=${DRIVER:0:LAST_NUM1}
+
+for DEV in `ls $DRIVER2[0-9]`
+do
+	if [ -e $DEV ]
+	then
+		if [ "$DEV" == "$DRIVER" ]
+		then
+			echo "$DEV $DRIVER"
+			echo "mount"
+			break
+		else
+			echo "$DEV $DRIVER"
+			echo "no mount"
+			exit
+		fi
+	fi
+done
+
+if ! mount | awk '{print $1}' | grep -q "$DEVNAME"
 then
 	while cat /proc/mounts | awk '{print $2}' | grep -q "$DIR" 
 	do
